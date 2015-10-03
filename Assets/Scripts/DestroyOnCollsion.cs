b2bf2ee12ef0cd4c756b40639497c15868303695
Jason C <jasoncarrete5@gmail.com>
2015-10-03 19:13:17 -0400

    Ball can bounce off walls and be deleted when it hits the ends

---
+++ Assets/Scripts/DestroyOnCollsion.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyOnCollsion : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collision occured");
+		Destroy(collision.gameObject);
+	}
+}
