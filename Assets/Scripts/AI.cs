e3309e3417f62ffb01e2f8d843ca5355e67f8340
inser7name <inser7name@gmail.com>
2015-10-04 07:09:34 -0400

    AI fixes

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -26,36 +26,17 @@ public class AI : MonoBehaviour {
 			if(Mathf.Abs(balls[0].transform.position.x - newPosition.x) <= 1.5)
 			    newPosition.x = balls[0].transform.position.x;
 			if(balls[0].transform.position.y - newPosition.y < -1.5)
-			{
 				newPosition.y -= 1.5f;
-				Debug.Log("Here0");
-			}
 			if(balls[0].transform.position.y - newPosition.y > 1.5)
-			{
 				newPosition.y += 1.5f;
-				Debug.Log("Here1");
-			}
 			if(balls[0].transform.position.x - newPosition.x < -1.5)
-			{
 				newPosition.x -= 1.5f;
-				Debug.Log("Here2");
-			}
 			if(balls[0].transform.position.x - newPosition.x > 1.5)
-			{
 				newPosition.x += 1.5f;
-<<<<<<< HEAD
-				Debug.Log("Here3");
-			}
-			//Debug.Log(balls[0].transform.position.x + "\t" + balls[0].transform.position.y);
-			//Debug.Log(transform.position.x + "\t" + transform.position.y);
-=======
-			//Debug.Log(balls[0].transform.position.x + "\t" + balls[0].transform.position.y);
->>>>>>> 3ce08858cb89dbfd9be86f4520a1bf76c63c6639
 			float x = newPosition.x;
 			float y = newPosition.y;
 			newPosition.z = (Mathf.Sqrt(-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + Mathf.Pow (radius, 2)) - centerZ);
 			transform.position = newPosition;
-			//Debug.Log(transform.position.x + "\t" + transform.position.y + "\t" + transform.position.z);
 			transform.rotation = Quaternion.Euler(new Vector3(180 + Mathf.Rad2Deg * Mathf.Atan (transform.position.y / transform.position.z),180 + Mathf.Rad2Deg * Mathf.Atan(transform.position.x / transform.position.z), 0));
 		}
 		if (framesTillSkip < 1)
