a335f206ce4d438e3a96df9e70e07c0a7810b8d1
inser7name <inser7name@gmail.com>
2015-10-03 20:15:42 -0400

    Made powerups

---
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpSpawner : MonoBehaviour {
+
+	public GameObject[] powerups;
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine(Spawn());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Spawn () 
+	{
+		while (true) 
+		{
+			yield return new WaitForSeconds (5);
+			int randNum = Random.Range (0, 25);
+			if (randNum < powerups.Length)
+				Instantiate (powerups [randNum], new Vector3 (0, 0, 0), new Quaternion (0, 0, 0, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour 
+{
+	public Vector3 rotate;
+	// Use this for initialization
+	void Start() 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		transform.Rotate (rotate);
+	}
+}
