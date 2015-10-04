ea4eaba2087e7bdeee0c3f5d94fef4bb8f9ab1a7
inser7name <inser7name@gmail.com>
2015-10-04 04:52:05 -0400

    AI

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	private GameObject[] balls;
+	public float centerZ;
+	public float radius;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		balls = GameObject.FindGameObjectsWithTag("Ball");
+		if(balls.Length > 0)
+		{
+			Vector3 newPosition = transform.position;
+			if(balls[0].transform.position.y < newPosition.y)
+				newPosition.y -= 1.5f;
+			if(balls[0].transform.position.y > newPosition.y)
+				newPosition.y += 1.5f;
+			if(balls[0].transform.position.x < newPosition.x)
+				newPosition.x -= 1.5f;
+			if(balls[0].transform.position.x > newPosition.x)
+				newPosition.x += 1.5f;
+			Debug.Log(balls[0].transform.position.x + "\t" + balls[0].transform.position.y);
+			float x = newPosition.x;
+			float y = newPosition.y;
+			newPosition.z = (Mathf.Sqrt(-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + Mathf.Pow (radius, 2)) - centerZ);
+			transform.position = newPosition;
+			Debug.Log(transform.position.x + "\t" + transform.position.y + "\t" + transform.position.z);
+			transform.rotation = Quaternion.Euler(new Vector3(180 + Mathf.Rad2Deg * Mathf.Atan (transform.position.y / transform.position.z),180 + Mathf.Rad2Deg * Mathf.Atan(transform.position.x / transform.position.z), 0));
+		}
+	}
+}
--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class PlayerMover : MonoBehaviour 
 {	
+	public float centerZ;
+	public float radius;
 	// Update is called once per frame
 	void Update () 
 	{
 		Vector3 newPosition = transform.position;
-		if (Input.GetKey ("w") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) + Mathf.Abs((float)(newPosition.z + 5)) <= 4.50 || newPosition.y < 0)) 
-			newPosition.y += 0.1f;
-		if (Input.GetKey ("s") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) + Mathf.Abs((float)(newPosition.z + 5)) <= 4.50 || newPosition.y > 0))
-			newPosition.y -= 0.1f;
-		if (Input.GetKey ("a") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) + Mathf.Abs((float)(newPosition.z + 5)) <= 4.50 || newPosition.x > 0))
-			newPosition.x -= 0.1f;
-		if (Input.GetKey ("d") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) + Mathf.Abs((float)(newPosition.z + 5)) <= 4.50 || newPosition.x < 0))
-			newPosition.x += 0.1f;
+		if (Input.GetKey ("w") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) <= radius || newPosition.y < 0)) 
+			newPosition.y += 0.15f;
+		if (Input.GetKey ("s") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) <= radius || newPosition.y > 0))
+			newPosition.y -= 0.15f;
+		if (Input.GetKey ("a") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) <= radius || newPosition.x > 0))
+			newPosition.x -= 0.15f;
+		if (Input.GetKey ("d") && (Mathf.Abs(newPosition.x) + Mathf.Abs(newPosition.y) <= radius || newPosition.x < 0))
+			newPosition.x += 0.15f;
 		float x = newPosition.x;
 		float y = newPosition.y;
-		//Debug.Log (-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + 56.25);
-		newPosition.z = -1 * (Mathf.Sqrt((-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + 20.25f)) + 2.75f);
+		newPosition.z = -1 * (Mathf.Sqrt(-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + Mathf.Pow (radius, 2)) - centerZ);
 		transform.position = newPosition;
 		transform.rotation = Quaternion.Euler(new Vector3(360 - Mathf.Rad2Deg * Mathf.Atan (transform.position.y / transform.position.z), Mathf.Rad2Deg * Mathf.Atan(transform.position.x / transform.position.z), 0));
 	}
