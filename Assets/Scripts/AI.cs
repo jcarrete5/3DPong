718087cec97ba9e815ef358c9a6f8a3fde3eefd1
inser7name <inser7name@gmail.com>
2015-10-04 07:04:23 -0400

    AI fixes

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -6,34 +6,55 @@ public class AI : MonoBehaviour {
 	private GameObject[] balls;
 	public float centerZ;
 	public float radius;
+	private int framesTillSkip;
 	// Use this for initialization
 	void Start () 
 	{
-
+		framesTillSkip = 10;
 	}
 	
 	// Update is called once per frame
 	void Update() 
 	{
 		balls = GameObject.FindGameObjectsWithTag("Ball");
-		if(balls.Length > 0)
+		if(balls.Length > 0 && framesTillSkip > 0)
 		{
+			framesTillSkip--;
 			Vector3 newPosition = transform.position;
-			if(balls[0].transform.position.y < newPosition.y)
+			if(Mathf.Abs(balls[0].transform.position.y - newPosition.y) <= 1.5)
+				newPosition.y = balls[0].transform.position.y;
+			if(Mathf.Abs(balls[0].transform.position.x - newPosition.x) <= 1.5)
+			    newPosition.x = balls[0].transform.position.x;
+			if(balls[0].transform.position.y - newPosition.y < -1.5)
+			{
 				newPosition.y -= 1.5f;
-			if(balls[0].transform.position.y > newPosition.y)
+				Debug.Log("Here0");
+			}
+			if(balls[0].transform.position.y - newPosition.y > 1.5)
+			{
 				newPosition.y += 1.5f;
-			if(balls[0].transform.position.x < newPosition.x)
+				Debug.Log("Here1");
+			}
+			if(balls[0].transform.position.x - newPosition.x < -1.5)
+			{
 				newPosition.x -= 1.5f;
-			if(balls[0].transform.position.x > newPosition.x)
+				Debug.Log("Here2");
+			}
+			if(balls[0].transform.position.x - newPosition.x > 1.5)
+			{
 				newPosition.x += 1.5f;
-			Debug.Log(balls[0].transform.position.x + "\t" + balls[0].transform.position.y);
+				Debug.Log("Here3");
+			}
+			//Debug.Log(balls[0].transform.position.x + "\t" + balls[0].transform.position.y);
+			//Debug.Log(transform.position.x + "\t" + transform.position.y);
 			float x = newPosition.x;
 			float y = newPosition.y;
 			newPosition.z = (Mathf.Sqrt(-(Mathf.Pow (x, 2)) - (Mathf.Pow (y, 2)) + Mathf.Pow (radius, 2)) - centerZ);
 			transform.position = newPosition;
-			Debug.Log(transform.position.x + "\t" + transform.position.y + "\t" + transform.position.z);
+			//Debug.Log(transform.position.x + "\t" + transform.position.y + "\t" + transform.position.z);
 			transform.rotation = Quaternion.Euler(new Vector3(180 + Mathf.Rad2Deg * Mathf.Atan (transform.position.y / transform.position.z),180 + Mathf.Rad2Deg * Mathf.Atan(transform.position.x / transform.position.z), 0));
 		}
+		if (framesTillSkip < 1)
+			framesTillSkip = 10;
 	}
 }
