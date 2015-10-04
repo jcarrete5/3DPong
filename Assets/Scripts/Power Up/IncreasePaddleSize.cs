3ce08858cb89dbfd9be86f4520a1bf76c63c6639
Jason C <jasoncarrete5@gmail.com>
2015-10-04 07:02:36 -0400

    Added win/lose sounds

--- Assets/Scripts/Power Up/IncreasePaddleSize.cs
+++ Assets/Scripts/Power Up/IncreasePaddleSize.cs
@@ -14,20 +14,12 @@ public class IncreasePaddleSize : MonoBehaviour
 		if(other.gameObject.GetComponent<BallMemory>().PlayerHitBallLast)
 		{
 			GameObject player = GameObject.FindGameObjectWithTag("Player");
-			StartCoroutine(Reset(player, player.transform.localScale));
-			player.transform.localScale = new Vector3(2, 2, 0);
+			player.transform.localScale += new Vector3(0.5f, 0.5f, 0);
 		}
 		else
 		{
 			GameObject ai = GameObject.FindGameObjectWithTag("AI");
-			StartCoroutine(Reset(ai, ai.transform.localScale));
-			ai.transform.localScale = new Vector3(2, 2, 0);
+			ai.transform.localScale += new Vector3(0.5f, 0.5f, 0);
 		}
-	}
-	
-	IEnumerator Reset(GameObject obj, Vector3 original)
-	{
-		yield return new WaitForSeconds(20);
-		obj.transform.localScale = original;
 	}
 }
